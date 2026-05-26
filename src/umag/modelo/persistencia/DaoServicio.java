8c587fbc239cf01fa5e543d268824bf42ab790bc
Castillo <Castillo@BROCASTLE>
2026-05-25 23:47:47 -0500

    Commit Guardado

--- src/umag/modelo/persistencia/DaoServicio.java
+++ src/umag/modelo/persistencia/DaoServicio.java
@@ -4,6 +4,7 @@
  */
 package umag.modelo.persistencia;
 
+import umag.modelo.entidad.Servicio;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -12,22 +13,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import umag.modelo.entidad.Prestador;
-import umag.modelo.entidad.Servicio;
 
 /**
  *
  * @author Castillo
  */
-
-public class DaoPrestador {
-    private String archivo = "Prestador.dat";
+public class DaoServicio {
+        private String archivo = "Prestador.dat";
     
-    public boolean guardar(Prestador prestador) throws FileNotFoundException, IOException, ClassNotFoundException{
+    public boolean guardar(Servicio servicio) throws FileNotFoundException, IOException, ClassNotFoundException{
         // aqui se guarda
         
-        List<Prestador> lista = listar();
-        lista.add(prestador);
+        List<Servicio> lista = listar();
+        lista.add(servicio);
         FileOutputStream file =new FileOutputStream(archivo);
         ObjectOutputStream fil =new ObjectOutputStream(file);
         fil.writeObject(lista);
@@ -36,30 +34,30 @@ public class DaoPrestador {
         return true;
     }
  
-    public boolean borrar(String id) throws IOException, ClassNotFoundException{
-        List<Prestador> lista = listar();
-        for (Prestador prestador : lista) {
-            
-            if(prestador.getId().equals(id)){
-                lista.remove(prestador);
-                break;
-            }
-        }
-        FileOutputStream file=new FileOutputStream(archivo);
-        ObjectOutputStream fil=new ObjectOutputStream(file);
-        fil.writeObject(lista);
-        fil.close();
-        file.close();
-        return true;
-    }
+    // public boolean borrar(String id) throws IOException, ClassNotFoundException{
+    //    List<Servicio> lista = listar();
+    //    for (Servicio servicio : lista) {
+    //        
+    //        if(servicio.getId().equals(id)){
+    //            lista.remove(prestador);
+    //            break;
+    //        }
+    //    }
+    //    FileOutputStream file=new FileOutputStream(archivo);
+    //    ObjectOutputStream fil=new ObjectOutputStream(file);
+    //    fil.writeObject(lista);
+    //    fil.close();
+    //    file.close();
+    //    return true;
+   // }
     
-    public List<Prestador> listar() throws IOException, ClassNotFoundException {
-        List<Prestador> lista=new ArrayList<>();
+    public List<Servicio> listar() throws IOException, ClassNotFoundException {
+        List<Servicio> lista=new ArrayList<>();
         FileInputStream file;
         try {
             file = new FileInputStream(archivo);
             ObjectInputStream entrada=new ObjectInputStream(file);
-            lista = (List<Prestador>) entrada.readObject();
+            lista = (List<Servicio>) entrada.readObject();
             
         } catch (FileNotFoundException ex) {
             System.out.println(" No existe archivo ");
--- src/umag/modelo/servicio/ServicioPrestador.java
+++ src/umag/modelo/servicio/ServicioPrestador.java
@@ -5,8 +5,10 @@
 package umag.modelo.servicio;
 
 
+import java.io.IOException;
 import umag.modelo.entidad.Prestador;
 import umag.modelo.entidad.Servicio;
+import umag.modelo.persistencia.DaoPrestador;
 
 /**
  *
@@ -14,17 +16,23 @@ import umag.modelo.entidad.Servicio;
  */
 public class ServicioPrestador {
     // Falta agregar el Dao
-    
+    DaoPrestador dao = new DaoPrestador();
         public boolean guardar(String id, int cédula, int teléfono, int edad, String nombre, String email, String dirección, String banco, boolean disponibilidad, Servicio servicio){
             int longitudCedula = String.valueOf(cédula).length();
         if (cédula <= 0 || longitudCedula < 8 || longitudCedula > 10 || servicio == null ) {
         return false;
         }else{
-            Prestador prestador = new Prestador(id, cédula, teléfono, edad, nombre, email, dirección, banco, disponibilidad, servicio);
-                    
-            return true;
+            try {
+                Prestador prestador =new Prestador(id, cédula, teléfono, edad, nombre, email, dirección, banco, disponibilidad, servicio);
+                dao.guardar(prestador);
+                return true;
+            } catch (IOException ex) {
+                System.out.println(" "+ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.out.println(" "+ex.getMessage());
+            }
+            return false;
         }
-        
     }
    //     public boolean actualizarDisponibilidad(String id, boolean disponibilidad) {
     // Le pides el prestador al DAO usando su ID
